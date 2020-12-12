71aee1bc43637ab0c924eef7619175e88589cc97
Raimon Esteve <raimonesteve@gmail.com>
2020-12-12 11:27:12 +0100

    Upgrade py3

--- __init__.py
+++ __init__.py
@@ -1,5 +1,4 @@
 #This file is part galatea blueprint for Flask.
-#The COPYRIGHT file at the top level of this repository contains 
+#The COPYRIGHT file at the top level of this repository contains
 #the full copyright notices and license terms.
-from galatea_file import *
-
+from .galatea_file import *
